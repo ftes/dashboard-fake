1d5fb4ed9279c9062761473a1acb00a8ad820398
Fredrik Teschke <f@ftes.de>
2016-12-02 23:39:09 +0100

    remove leading dot from file names in loops

--- create-index.sh
+++ create-index.sh
@@ -6,7 +6,7 @@ curl -XPUT "$BASE"
 LOC_DATA='{ "properties" : { "location" : { "type" : "geo_point" } } }'
 
 cd data
-for file in ./*; do
+for file in *; do
     echo "curl -XPUT \"$BASE/_mapping/$file\" -d \"$LOC_DATA\""
     curl -XPUT "$BASE/_mapping/$file" -d "$LOC_DATA"
 done
--- upload-data.sh
+++ upload-data.sh
@@ -1,7 +1,7 @@
 source params
 
 cd data
-for file in ./*; do
+for file in *; do
     echo "curl -XPOST \"$BASE/$file/_bulk?refresh\" --data-binary \"@$file\" > /dev/null"
 	curl -XPOST "$BASE/$file/_bulk?refresh" --data-binary "@$file" > /dev/null
 done
