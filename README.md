4be367689222f5ab0cdb2af2050cc8513f559ceb
Fredrik Teschke <f@ftes.de>
2016-12-02 09:40:35 +0100

    fake all schemas, and cross-reference ids

---
+++ README.md
@@ -0,0 +1,3 @@
+# Usage
+1. `yarn install` (or `npm install`)
+2. `npm start`
--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 let fs = require('fs');
 let jsf = require('json-schema-faker');
 let yaml = require('js-yaml');
+let deref = require('json-schema-deref-sync');
+let path = require('path');
+
+const outDir = 'data';
+
+
+// add custom generator for integer timestamp
+jsf.extend('faker', function(faker) {
+    faker.locale = 'de'; // or any other language
+    let ids = {};
+    function getNewId(type) {
+        let currentId = ids[type] === undefined ? -1 : ids[type];
+        ids[type] = ++currentId;
+        return currentId;
+    }
+    function getExistingId(type) {
+        return faker.random.number({min: 0, max: ids[type]});
+    }
+    faker.custom = {
+        unixTimestamp: function(years, refDate) {
+            return Math.floor(faker.date.past(years, refDate).getTime() / 1000);
+        },
+        id: function(options) {
+            let type = options.type;
+            let reference = options.reference || false;
+            return reference ? getExistingId(type) : getNewId(type);
+        }
+  };
+  return faker;
+});
 
 let api = yaml.safeLoad(fs.readFileSync('./api.yaml'));
-let schema = {
-	type: 'array',
-	items: api.definitions.CreateCustomerRequestDto,
-	minItems: 5,
-	maxItems: 5,
-	uniqueItems: true
-};
+api = deref(api);
 
-let fake = jsf(schema);
+let dtosToGenerate = {
+    CustomerDto: 10,
+    ProviderDto: 5,
+    FileDto: 1000,
+    Chunk: 10000,
+    SharingDto: 1000,
+};
 
 function buildId(i) {
 	return {
@@ -21,8 +51,25 @@ function buildId(i) {
 	};
 }
 
-let i = 0;
-for (let item of fake) {
-	console.log(JSON.stringify(buildId(i++)));
-	console.log(JSON.stringify(item));
+for (let dto in dtosToGenerate) {
+    let n = dtosToGenerate[dto];
+    let schema = {
+	    type: 'array',
+	    uniqueItems: true,
+        items: api.definitions[dto],
+        minItems: n,
+        maxItems: n,
+    };
+    let fake = jsf(schema);
+
+    let i = 0;
+    let file = fs.openSync(path.join(outDir, dto), 'w');
+    for (let item of fake) {
+        fs.writeSync(file, JSON.stringify(buildId(i++)) + '\n');
+        fs.writeSync(file, JSON.stringify(item) + '\n');
+    }
 }
+
+
+
+
